5c08d52bf9bc5970f20575a390f1929be97a324e
gam0022 <gam0022@gmail.com>
2015-09-21 01:18:43 +0900

    秘伝の my.cnf (効果なし)

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -av --include="*/" --include="*.go" --include="*.conf" --exclude="*" './' 'isucon@107.167.176.153:/home/isucon'
+rsync -av --include="*/" --include="*.go" --include="*.conf" --include="*.cnf" --exclude="*" './' 'isucon@107.167.176.153:/home/isucon'
