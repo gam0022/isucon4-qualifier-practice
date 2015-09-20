12d0e728d760b705b0843f440524aa46dbf4e65d
gam0022 <gam0022@gmail.com>
2015-09-20 17:59:09 +0000

    add index for isBannedIP

--- sql/schema.sql
+++ sql/schema.sql
@@ -12,5 +12,6 @@ CREATE TABLE IF NOT EXISTS `login_log` (
   `login` varchar(255) NOT NULL,
   `ip` varchar(255) NOT NULL,
   `succeeded` tinyint NOT NULL,
-  index(user_id, succeeded, id)
+  index(user_id, succeeded, id),
+  index(ip, succeeded, id)
 ) DEFAULT CHARSET=utf8;
